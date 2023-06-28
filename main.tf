c62ac895af0faa2a4250e764f28ab7d15c968622
noel garcia <noel.garcia@intellecteu.com>
2023-06-28 18:08:49 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,249 @@
+# This is a terraform project to deploy on GCP a polygon supernet on top of compute engine instances.
+
+variable "project" {
+  description = "The project where we want to deploy"
+  type        = string
+  default     = "polygon-060623"
+}
+variable "base_instance_type" {
+  description = "The type of instance that we're going to use"
+  type        = string
+  default     = "e2-medium"
+}
+variable "company_name" {
+  description = "The name of the company for this particular deployment"
+  type        = string
+  default     = "IEU"
+}
+
+variable "create_ssh_key" {
+  description = "Should a new ssh key be created or should we use the devnet_key_value"
+  type        = bool
+  default     = true
+}
+
+variable "deployment_name" {
+  description = "The unique name for this particular deployment"
+  type        = string
+  default     = "gp23-poc3"
+}
+
+variable "devnet_key_value" {
+  description = "The public key value to use for the ssh key. Required when create_ssh_key is false"
+  type        = string
+  default     = ""
+}
+
+variable "devnet_private_subnet" {
+  description = "The cidr block for the private subnet in our VPC"
+  type        = list(string)
+  default     = ["10.10.64.0/22", "10.10.68.0/22", "10.10.72.0/22", "10.10.76.0/22"]
+}
+
+variable "devnet_public_subnet" {
+  description = "The cidr block for the public subnet in our VPC"
+  type        = list(string)
+  default     = ["10.10.0.0/22", "10.10.4.0/22", "10.10.8.0/22", "10.10.12.0/22"]
+}
+
+variable "devnet_vpc_block" {
+  description = "The cidr block for our VPC"
+  type        = string
+  default     = "10.10.0.0/16"
+}
+
+variable "environment" {
+  description = "The environment for deployment for this particular deployment"
+  type        = string
+  default     = "devnet"
+}
+
+variable "fullnode_count" {
+  description = "The number of full nodes that we're going to deploy"
+  type        = number
+  default     = 0
+}
+
+variable "geth_count" {
+  description = "The number of geth nodes that we're going to deploy"
+  type        = number
+  default     = 1
+  validation {
+    condition = (
+      var.geth_count == 0 || var.geth_count == 1
+    )
+    error_message = "There should only be 1 geth node, or none (if you are using another public L1 chain for bridge)."
+  }
+}
+
+variable "http_rpc_port" {
+  description = "The TCP port that will be used for http rpc"
+  type        = number
+  default     = 10002
+}
+
+variable "network_acl" {
+  description = "Which CIDRs should be allowed to access the explorer and RPC"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "node_storage" {
+  description = "The size of the storage disk attached to full nodes and validators"
+  type        = number
+  default     = 10
+}
+
+variable "rootchain_rpc_port" {
+  description = "The TCP port that will be used for rootchain (for bridge)"
+  type        = number
+  default     = 8545
+}
+
+variable "route53_zone_id" {
+  description = "The ID of the hosted zone to contain the CNAME record to our LB"
+  type        = string
+  default     = ""
+}
+
+variable "owner" {
+  description = "The main point of contact for this particular deployment"
+  type        = string
+  default     = "user@email.com"
+}
+
+variable "private_network_mode" {
+  description = "True if vms should bey default run in the private subnets"
+  type        = bool
+  default     = true
+}
+
+variable "region" {
+  description = "The region where we want to deploy"
+  type        = string
+  default     = "europe-west1"
+}
+
+variable "validator_count" {
+  description = "The number of validators that we're going to deploy"
+  type        = number
+  default     = 4
+}
+
+variable "zones" {
+  description = "The availability zones for deployment"
+  type        = list(string)
+  default     = ["europe-west1-b", "europe-west1-c", "europe-west1-d"]
+}
+
+
+
+
+
+locals {
+  network_type = "edge"
+  base_ami     = "ubuntu-os-cloud/ubuntu-2204-lts"
+  base_dn      = format("%s-%s-%s-private", var.deployment_name, local.network_type, var.company_name)
+  base_id      = format("%s-%s", var.deployment_name, var.environment)
+  common_labels = {
+    environment    = lower(var.environment)
+    network        = lower(local.network_type)
+    owner          = lower(var.owner)
+    deployment_name = lower(var.deployment_name)
+    base_dn         = lower(local.base_dn)
+    name           = lower(local.base_id)
+  }
+}
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "google" {
+  project     = var.project
+  region      = var.region
+}
+
+
+resource "google_compute_network" "network" {
+  name = "${local.base_id}-network"
+  # labels = local.common_labels
+  auto_create_subnetworks = false
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  count = length(var.zones)
+
+  name          = "${local.base_id}-subnet-${count.index}"
+  ip_cidr_range = "10.0.${count.index}.0/24"
+  region        = var.region
+  network       = google_compute_network.network.self_link
+  private_ip_google_access = true
+
+  depends_on = [google_compute_network.network]
+
+  secondary_ip_range {
+    range_name    = "pods"
+    ip_cidr_range = "10.1.${count.index}.0/24"
+  }
+
+  secondary_ip_range {
+    range_name    = "services"
+    ip_cidr_range = "10.2.${count.index}.0/24"
+  }
+}
+
+
+resource "google_compute_instance" "validator" {
+  count = var.validator_count
+  name         = "${local.base_id}-validator-${count.index}"
+  machine_type = var.base_instance_type
+  zone         = var.zones[count.index % length(var.zones)]
+  boot_disk {
+    initialize_params {
+      image = local.base_ami
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.network.self_link
+    subnetwork = google_compute_subnetwork.subnet[count.index % length(var.zones)].self_link
+  }
+  labels       = local.common_labels
+}
+
+
+# output "gcp_lb_int_rpc_domain" {
+#   value = module.elb.gcp_lb_int_rpc_domain
+# }
+
+# output "gcp_lb_ext_domain" {
+#   value = module.elb.gcp_lb_ext_rpc_domain
+# }
+
+# output "gcp_lb_ext_geth_domain" {
+#   value = module.elb.gcp_lb_ext_rpc_geth_domain
+# }
+
+# output "base_dn" {
+#   value = local.base_dn
+# }
+# output "base_id" {
+#   value = local.base_id
+# }
+# output "pk_ansible" {
+#   value     = module.ec2.pk_ansible
+#   sensitive = true
+# }
+
+# output "geth_private_ip" {
+#   value     = module.ec2.geth_private_ips[0]
+# }
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+# deployment_name = "devnet01"
+# company_name = "company"
+owner = "noel-garcia"
+fullnode_count = 0
