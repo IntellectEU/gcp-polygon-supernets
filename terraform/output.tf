4147edff1d85883b1132cd38dbdba1f9326a5a1b
DiMSaraiva <d1_saraiva@hotmail.com>
2023-09-14 16:58:09 +0100

    Fix outputs, moving directory

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,6 +6,15 @@ output "validator_private_ips" {
   value = google_compute_instance.validator[*].network_interface.0.network_ip
 }
 
+output "service_account_email" {
+  value = google_service_account.polygon_sa.email
+}
+
+output "polygon_sa_key" {
+  value = base64decode(google_service_account_key.polygon_sa.private_key)
+  sensitive = true
+}
+
 ### Further development: Not being used right now ###
 # output "gcp_lb_int_rpc_domain" {
 #   value = module.elb.gcp_lb_int_rpc_domain
---
+++ terraform/sa.tf
@@ -0,0 +1,26 @@
+data "google_service_account" "polygon_sa" {
+  account_id = google_service_account.polygon_sa.name
+}
+
+resource "google_service_account" "polygon_sa" {
+  account_id   = "polygon-sa"
+  display_name = "polygon-sa"
+}
+
+resource "google_service_account_key" "polygon_sa" {
+  service_account_id = google_service_account.polygon_sa.name
+}
+
+resource "google_project_iam_binding" "polygon_sa_binding" {
+  project = var.project
+  role    = "roles/editor"
+
+  members = [
+    "serviceAccount:${google_service_account.polygon_sa.email}"
+  ]
+}
+
+resource "local_file" "polygon_sa_key" {
+  filename = "polygon-sa.json"
+  content  = "${base64decode(google_service_account_key.polygon_sa.private_key)}"
+}
